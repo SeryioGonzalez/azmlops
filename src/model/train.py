d70e384304b9c7a20f7d2d984762f4241c8d2db7
seryiogonzalez <sergio.gonzalez.jimenez@gmail.com>
2023-10-19 11:03:26 +0200

    Linting

--- src/model/train.py
+++ src/model/train.py
@@ -32,6 +32,7 @@ def get_csvs_df(path):
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
